6138384b49891c5fdf4eaf029371d756a15df534
studiomac-142 <studiomac-142@studiomac-142s-iMac.local>
2022-05-09 17:06:48 -0700

    styling leave a reply component

--- src/pages/rock-shrimp-page/rock-shrimp-page.component.jsx
+++ src/pages/rock-shrimp-page/rock-shrimp-page.component.jsx
@@ -2,11 +2,14 @@ import React from "react";
 
 import { Link } from "react-router-dom";
 
+import NewsSideBar from "../../components/news-sidebar/news-sidebar.component";
+
 import './rock-shrimp-page.styles.scss';
 
 
 const RockShrimpPage = () => (
     <div className="rock-shrimp">
+
         <div className="header-options">
             <p>Menu</p>
             <p>Cart</p>
@@ -20,6 +23,7 @@ const RockShrimpPage = () => (
             <h1>Black Spaghetti with Rock Shrimp</h1>
             <p className="date">APRIL 20, 2015 IN <Link to='/news' className="linked">ITALIAN</Link></p>
         </div>
+
         <div className="rockshrimp-blog">
             <div className="content">
                 <p>Lorem ipsum dosectetur adipisicing elit, sed do.Lorem ipsum dolor sit amet, consectetur Nulla fringilla purus at leo dignissim congue. Mauris</p>
@@ -59,13 +63,46 @@ const RockShrimpPage = () => (
                     <p>APRIL 18, 2015</p>
                 </div>
                 <div className="like-2">
-                   <img src="https://themegoods-cdn-pzbycso8wng.stackpathdns.com/grandrestaurant/demo1/wp-content/uploads/2020/12/view-of-stylish-empty-cafe-with-arranged-tables-an-AA9VFEU-150x150.jpg" />
-                   <a href="amazingdining">Amazing Dining Experience Begins</a>
-                   <p>APRIL 16, 2015</p>
+                    <img src="https://themegoods-cdn-pzbycso8wng.stackpathdns.com/grandrestaurant/demo1/wp-content/uploads/2020/12/view-of-stylish-empty-cafe-with-arranged-tables-an-AA9VFEU-150x150.jpg" />
+                    <a href="amazingdining">Amazing Dining Experience Begins</a>
+                    <p>APRIL 16, 2015</p>
+                </div>
+                <div className="like-3">
+                    <img src="https://themegoods-cdn-pzbycso8wng.stackpathdns.com/grandrestaurant/demo1/wp-content/uploads/2014/07/image-from-rawpixel-id-444728-jpeg-150x150.jpg" />
+                    <a href="premiumrecipe">Our Premium Food Recipe</a>
+                    <p>APRIL 9, 2015</p>
+                </div>
+                <div className="like-4">
+                    <img src="https://themegoods-cdn-pzbycso8wng.stackpathdns.com/grandrestaurant/demo1/wp-content/uploads/2014/07/chiles-en-nogada-dish-on-mexican-independence-day-PVRJURW-150x150.jpg" />
+                    <a href="combiofcooking">Life is a combination of cooking</a>
+                    <p>APRIL 5, 2015</p>
                 </div>
             </div>
+            <div className="about-author">
+                <h3>ABOUT THE AUTHOR</h3>
+                <img src="https://secure.gravatar.com/avatar/db6f032dce962144efc9b625779461a1?s=160&d=mm&r=g" />
+                <div className="author-detail">
+                    <h5>John Philipe</h5>
+                    <p>Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo</p>
+                    <p>consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat.</p>
+                </div>
+            </div>
+            <div className="leave-a-reply">
+                <h3>LEAVE A REPLY</h3>
+                <p>Your email address will not be published. Required fields are marked *</p>
+                <textarea id="comment" name="comment" cols="45" rows="8" maxlength="65525" required="required"></textarea>
+                <input className="author" placeholder="Name" id="author" name="author" type="text" value size="30" maxlength="245" required="required"></input>
+                <input className="email" placeholder="Email" id="email" name="email" type="email" value size="30" maxlength="100" aria-describedby="email-notes" required="required"></input>
+                <input className="website" placeholder="Website" id="url" name="url" type="url" value size="30" maxlength="200" required="required"></input>
+                <input className="checkbox" id="wp-comment-cookies-consent" name="wp-comment-cookies-consent" type="checkbox" value="yes"></input>
+                <input id="submit" name="submit" type="submit" className="submit" value="Post Comment"></input>
+
+            </div>
         </div>
-    </div>
+        <NewsSideBar />
+    </div> 
+    
+
 );
 
 
